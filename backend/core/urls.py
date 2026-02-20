c9b1bf185b30fab2cb0409979c5d2e69257f7fbd
ss0900 <psypsy1234zz@gmail.com>
2026-02-20 13:52:43 +0900

    회사 등록 및 수정 시 로고 필드 저장 오류 해결

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -25,6 +25,11 @@ urlpatterns += [
     path("companies/<int:pk>/", belong_view.CompanyViewSet.as_view({
         "get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"
     }), name="companies-detail"),
+    path(
+        "companies/<int:pk>/upload-logo/",
+        belong_view.CompanyViewSet.as_view({"post": "upload_logo"}),
+        name="companies-upload-logo",
+    ),
 
     path("departments/", belong_view.DepartmentViewSet.as_view({"get": "list", "post": "create"}), name="departments-list"),
     path("departments/org-chart/", belong_view.DepartmentViewSet.as_view({"get": "org_chart"}), name="departments-org-chart"),
