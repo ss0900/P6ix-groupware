bf70af810ac8c0ba51a6dcdcb99b1a6f5c98b940
jiayou0518 <jiayou0518@naver.com>
2026-02-24 15:24:04 +0900

    머지해결

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://127.0.0.1:8001',
         changeOrigin: true,
       },
     },
